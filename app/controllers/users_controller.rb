713a2ab5d82235d80490eb73cf9764d45d9809f4
lt <logantanous@gmail.com>
2018-05-04 13:03:31 -0700

    Add flash messages for signing up, signing in and signing out.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,9 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:alert] = "Successfully signed up"
       redirect_to '/'
+
     else
       redirect_to '/signup'
     end
