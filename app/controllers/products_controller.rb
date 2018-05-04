9f2de7012198de5a24198bd1927659222231e7ea
lt <logantanous@gmail.com>
2018-05-04 15:08:32 -0700

    Add product update and delete functionality for admins.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,11 +35,18 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
-  # def destroy
-  #   @product = Product.find(params[:id])
-  #   @product.destroy
-  #   redirect_to products_path
-  # end
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :update
+    end
+  end
 
   private
   def product_params
