bae701a5491f4769e336b99f6975dab7e783c698
lt <logantanous@gmail.com>
2018-05-04 14:43:31 -0700

    get delete functionality working for admins

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,6 +28,19 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    @product.save
+    redirect_to products_path
+  end
+
+  # def destroy
+  #   @product = Product.find(params[:id])
+  #   @product.destroy
+  #   redirect_to products_path
+  # end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price)
